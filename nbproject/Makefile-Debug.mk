9744a80b734798722dfa54686694310509bea672
Kevin Becker <kbecke35@ford.com>
2018-11-11 08:53:19 -0500

    Added 1st draft of NetBeans v8.0.2 project

---
+++ nbproject/Makefile-Debug.mk
@@ -0,0 +1,90 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=Cygwin-Windows
+CND_DLIB_EXT=dll
+CND_CONF=Debug
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/_ext/2066400048/cwsw_lib.o \
+	${OBJECTDIR}/ut/cwsw_lib_test.o
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cwsw_lib.git.exe
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cwsw_lib.git.exe: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cwsw_lib.git ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/_ext/2066400048/cwsw_lib.o: /cygdrive/C/_Projects/pdev2/cwsw_lib.git/cwsw_lib/src/cwsw_lib.c 
+	${MKDIR} -p ${OBJECTDIR}/_ext/2066400048
+	${RM} "$@.d"
+	$(COMPILE.c) -g -DXPRJ_NB_${CONF} -Icwsw_lib -Icfg -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/2066400048/cwsw_lib.o /cygdrive/C/_Projects/pdev2/cwsw_lib.git/cwsw_lib/src/cwsw_lib.c
+
+${OBJECTDIR}/ut/cwsw_lib_test.o: ut/cwsw_lib_test.c 
+	${MKDIR} -p ${OBJECTDIR}/ut
+	${RM} "$@.d"
+	$(COMPILE.c) -g -DXPRJ_NB_${CONF} -Icwsw_lib -Icfg -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/ut/cwsw_lib_test.o ut/cwsw_lib_test.c
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cwsw_lib.git.exe
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
